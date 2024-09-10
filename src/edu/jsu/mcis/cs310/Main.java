bda44d1765efcd3365ad999fd58f5192c235e444
CaydenT <ctucker18@stu.jsu.edu>
2024-09-10 07:38:05 -0500

    Cleaned up Main class code

--- src/edu/jsu/mcis/cs310/Main.java
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -5,30 +5,10 @@ public class Main {
     public static void main(String[] args) {
         
         Main m = new Main();
-        /*String[] messages = {m.getGreeting(), "I'm a little teapot", "Hey Diddle Diddle", "Hickory Dickory Dock"};
-        
-        for (String message : messages){
-        System.out.println(message);
-        System.out.println(m.reverse(message));
-        }*/
-        /* Main m = new Main();
         String message = m.getGreeting();
         
         System.out.println(message);
-        System.out.println(m.reverse(message));*/
-        String[] messages = {
-            m.getGreeting(),
-            "I'm a Little Teapot",
-            "Hey Diddle Diddle",
-            "Hickory Dickory Dock"
-        };
-        
-        // Print each string and its reversed version
-        for (String message : messages) {
-            System.out.println("Original: " + message);
-            System.out.println("Reversed: " + m.reverse(message));
-            System.out.println(); // Add a blank line for better readability
-        }
+        System.out.println(m.reverse(message));
         
     }
     
@@ -37,16 +17,10 @@ public class Main {
     }
     
     public String reverse(String message) {
-        /*StringBuilder sb = new StringBuilder(message);
-        sb.reverse();
-        return sb.toString();*/
-        //return message;
         StringBuilder sb = new StringBuilder(message);
-        
-        // Reverse the characters using StringBuilder's reverse() method
+
         sb.reverse();
         
-        // Return the reversed string
         return sb.toString();
     }
     
